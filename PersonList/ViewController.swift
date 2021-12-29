c3a06d80eb23e36fa6e17f7b37a8fc5ef09022fe
Elisei Mishchenko <esm.mail@icloud.com>
2021-12-29 15:30:06 +0300

    Initial Commit

---
+++ PersonList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonList
+//
+//  Created by Елисей Мищенко on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
