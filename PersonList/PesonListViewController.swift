d5d2c019a4f4b4e7c416428a4998ac725986d97f
Elisei Mishchenko <esm.mail@icloud.com>
2021-12-29 16:52:11 +0300

    Creating a new file and creating a DataManager class

---
+++ PersonList/PesonListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PesonListViewController.swift
+//  PersonList
+//
+//  Created by Елисей Мищенко on 29.12.2021.
+//
+
+import UIKit
+
+class PesonListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
